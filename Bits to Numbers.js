e9746a42a69020fefc070fc74aaf833bf31ee26b
ArvinLleva <arvin.lleva@yahoo.com>
2019-10-11 00:10:32 -0700

    updated Bits to Numbers.js

--- Bits to Numbers.js
+++ Bits to Numbers.js
@@ -7,16 +7,21 @@ const bitToNum = bitString => {
     let bit = parseInt(bitString[i]);
     bitArr.push(bit);
 
-    if (bit) {
+    if (bit == 1) {
       let num = Math.pow(2, bitArr.length -1);
       console.log(num)
       sum += num;
+    } else if (bit == 0) {
+      continue;
+    } else {
+      return 'please enter a series of bits'
     }
   }
-  console.log(bitArr)
   return sum;
 }
 
-console.log(bitToNum("111"));
+console.log(bitToNum("1"));
 console.log(bitToNum("101"));
-console.log(bitToNum("1110"));
+console.log(bitToNum("100"));
+console.log(bitToNum("1000"));
+console.log(bitToNum("100100"));
